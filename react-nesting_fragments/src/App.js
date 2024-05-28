44d05cb958b1288eaf94bc5488155febb7d7b8ba
sara <sarah.abiidar@gmail.com>
2024-05-28 16:38:26 +0200

    use fragments

---
+++ react-nesting_fragments/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./styles.css";
+import Box from "./components/Box";
+
+export default function App() {
+  return <Boxes />;
+}
+
+function Boxes() {
+  return (
+    <>
+      <Box color="#007bff" />
+      <Box color="#fc3" />
+      <Box color="#ff3333" />
+    </>
+  );
+}
+
+/*export default function App() {
+  return (
+    <main className="flex-container">
+      <Box color="#007bff" />
+      <Box color="#fc3" />
+      <Box color="#ff3333" />
+    </main>
+  );
+}*/
