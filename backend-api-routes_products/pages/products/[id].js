fcf5a9d394a1e10a94d56dc75ac19072a28c221d
sara <sarah.abiidar@gmail.com>
2024-06-18 23:40:08 +0200

    use useRouter to access the id query parameter

--- backend-api-routes_products/pages/products/[id].js
+++ backend-api-routes_products/pages/products/[id].js
@@ -1,7 +1,30 @@
-import { getProductById } from "../../../services/productServices";
+import useSWR from "swr";
+import { useRouter } from "next/router";
 
-export default function handler(request, response) {
-  const { id } = request.querry;
-  const product = getProductById(id);
-  response.status(200).json(product);
+async function fetcher(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+export default function ProductDetails() {
+  const router = useRouter();
+  const { id } = router.query;
+  const { data: product } = useSWR(`/api/products/${id}`, fetcher);
+
+  if (!product) {
+    return "loading";
+    /*useSWR sends the fetch request.
+    Initially, data is undefined because the request hasn't resolved yet. 
+    we shoud wirte this if condition*/
+  }
+  return (
+    <div>
+      <p>{product.name}</p>
+      <p>{product.description}</p>
+      <p>{product.price}</p>
+      <p>{product.currency}</p>
+      <p>{product.category}</p>
+    </div>
+  );
 }
