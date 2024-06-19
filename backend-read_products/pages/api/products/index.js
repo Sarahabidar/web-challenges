821fdd8790983ad636953dd0d8a0629d59d1a556
sara <sarah.abiidar@gmail.com>
2024-06-19 23:40:33 +0200

    read from data base, create schema , use populate method

---
+++ backend-read_products/pages/api/products/index.js
@@ -0,0 +1,10 @@
+import dbConnect from "../../../db/connect.js";
+import Product from "../../../db/models/Product.js";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  if (request.method === "GET") {
+    const products = await Product.find();
+    response.status(200).json(products);
+  }
+}
