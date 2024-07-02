f6e04ec962daac95e459acb3bb05f0b67dc9c23b
Sara <sarah.abiidar@gmail.com>
2024-07-02 10:50:23 +0200

    add comment

--- react-state-2_colored-number/src/components/Counter.js
+++ react-state-2_colored-number/src/components/Counter.js
@@ -3,6 +3,7 @@ import ColoredNumber from "./ColoredNumber";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
+  //useState für change the state
   return (
     <div className="counter">
       <h1>Colored Number</h1>
